4fe20a2f9cad4854649c632553a7aeea29937a3e
Dhruv Makwana <dc_mak@outlook.com>
2024-11-22 18:22:14 +0000

    Finish formal intro, start formal grammar
    Finished bidir, precision, monadic, arrays in formal intro. Have to
    write worse & faster. Started on CN grammar.
    08:00 10,133
    09:20 10,436
    12:10 10,822
    13:45 10,909
    15:55 11,404
    17:05 11,896 (break)
    17:31 11,896
    17:48 12,040
    18:20 12,220

--- src/code/dead_code.py
+++ src/code/dead_code.py
@@ -1,13 +1,15 @@
-# $ \{ \} $
-if x >= 5:
-    # $ \{ \mathrm{x} \geq 5 \} $
-    if x < 4:
-        # $ \{ \mathrm{x} \geq 5 , \mathrm{x} < 4 \} \Rightarrow \mathsf{false} $
-         # dead code
-        raise Exception('UB')
+def gt_zero()
+    # $ \vdash \{ \} \; \mathrm{gt\_zero()}  \; \{ \mathsf{ret} \mid \mathsf{ret} > 0 \} $
+    x = randint()
+    if x >= 5:
+        # $ \{ \mathrm{x} \geq 5 \} $
+        if x < 4:
+            # $ \{ \mathrm{x} \geq 5 , \mathrm{x} < 4 \} \Rightarrow \mathsf{false} $
+            # dead code
+            raise Exception('UB')
+        else:
+            # $ \{ \mathrm{x} \geq 5 , \mathrm{x} \geq 4 \} $
+            return x
     else:
-        # $ \{ \mathrm{x} \geq 5 , \mathrm{x} \geq 4 \} $
-        pass
-else:
-    # $ \{ \mathrm{x} < 5 \} $
-    pass
+        # $ \{ \mathrm{x} < 5 \} $
+        return 1
