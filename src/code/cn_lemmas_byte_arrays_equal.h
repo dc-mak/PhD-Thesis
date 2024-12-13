8e0ee8475053daf5a37865acd854c94c46eb899b
Dhruv Makwana <dc_mak@outlook.com>
2024-12-13 00:32:47 +0000

    Finish VIP impl, start on epilogue
    Distracted in the morning by thinking about the preprocessor, and other
    implementation related things. Should be clear of that now (hopefully).
    Need to keep a better track of the word count at different points.
    11:00 35,216 (preprocessing distraction)
    15:15 35,216 (call, dinner, distraction)
    00:30 36,285

---
+++ src/code/cn_lemmas_byte_arrays_equal.h
@@ -0,0 +1,18 @@
+/*@
+lemma byte_arrays_equal(pointer x, pointer y, u64 n)
+
+requires
+    take X = each (u64 i; 0u64 <= i && i < n)
+             { Owned(array_shift<unsigned char>(x, i)) };
+    take Y = each (u64 i; 0u64 <= i && i < n)
+             { Owned(array_shift<unsigned char>(y, i)) };
+    each (u64 i; 0u64 <= i && i < n) { X[i] == Y[i] };
+
+ensures
+    take XR = each (u64 i; 0u64 <= i && i < n )
+              { Owned(array_shift<unsigned char>(x, i)) };
+    take YR = each (u64 i; 0u64 <= i && i < n )
+              { Owned(array_shift<unsigned char>(y, i)) };
+    X == XR; Y == YR;
+    XR == YR;
+@*/
