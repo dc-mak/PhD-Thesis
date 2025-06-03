68a9ab22463dbbccb1cb73e90f25b5703a477308
Dhruv Makwana <dc-mak@users.noreply.github.com>
2025-06-03 17:50:37 +0100

    Fix #33, split up C and Core code

--- src/code/append_plain.c
+++ src/code/append_plain.c
@@ -4,10 +4,12 @@ struct int_list {
 
 struct int_list* IntList_append(
     struct int_list *xs, struct int_list *ys)
-{ if (xs == 0) {
+{
+  if (xs == 0) {
     return ys;
   } else {
-    struct int_list *new_tail =
-        IntList_append(xs->tail, ys);
+    struct int_list *new_tail = IntList_append(xs->tail, ys);
     xs->tail = new_tail;
-    return xs; } }
+    return xs;
+  }
+}
