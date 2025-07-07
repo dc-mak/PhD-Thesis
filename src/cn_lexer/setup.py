233030caa3b4a3fad282e19ca0e6150d769e0349
Dhruv Makwana <dc-mak@users.noreply.github.com>
2025-07-07 19:03:05 +0100

    Use CN Spec Lexer for CN minted commands

--- src/cn_lexer/setup.py
+++ src/cn_lexer/setup.py
@@ -7,6 +7,7 @@ setup(
     entry_points={
         "pygments.lexers": [
             "cn = cn_lexer.cn_lexer:CNLexer",
+            "cn-spec = cn_lexer.cn_spec_lexer:CNSpecLexer",
         ],
     },
 )
