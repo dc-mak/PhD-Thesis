0a5c0e0245c7bcb006a3cc961e308cd8dd2c9b66
Dhruv Makwana <dc-mak@users.noreply.github.com>
2025-08-04 18:32:01 +0100

    Delete Rocq files

--- src/code/PCUICGuard.v
+++
@@ -1,7 +0,0 @@
-Inductive FixCoFix : Type := Fix | CoFix.
-
-Axiom guard : FixCoFix -> global_env_ext -> context
-      -> mfixpoint term -> Prop.
-
-Definition fix_guard := guard Fix.
-Definition cofix_guard := guard CoFix.
