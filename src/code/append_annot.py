8ff379cd68fcfed761b30a0e68062bd863cb6134
Dhruv Makwana <dc-mak@users.noreply.github.com>
2025-09-11 13:21:33 +0100

    Address Peter feedack on Intro chapter

--- src/code/append_annot.py
+++ src/code/append_annot.py
@@ -5,8 +5,8 @@ if (xs == NULL) then
   return ys
   # $\{ \mathrm{list}(\mathsf{ys}, l_2) \}$
 else
-  # $\{ l_1 = v::l_1' \wedge{} xs \mapsto v \ast{} (\mathsf{xs} + 1) \mapsto{} \mathsf{xs'} \ast{} \mathrm{list}(\mathsf{xs'}, l_1')  \ast{} \mathrm{list}(\mathsf{ys}, l_2) \}$
+  # $\{ l_1 = v \Colon{} l_1' \wedge{} xs \mapsto v \ast{} (\mathsf{xs} + 1) \mapsto{} \mathsf{xs'} \ast{} \mathrm{list}(\mathsf{xs'}, l_1')  \ast{} \mathrm{list}(\mathsf{ys}, l_2) \}$
   new_tail = append(xs->tail, ys)
-  # $\{ l_1 = v::l_1' \wedge{} xs \mapsto{} v \ast{} (xs + 1) \mapsto{} \mathsf{xs'} \ast{}  \mathrm{list}(\mathsf{new\_tail}, l_1' @ l_2) \}$
+  # $\{ l_1 = v \Colon{} l_1' \wedge{} xs \mapsto{} v \ast{} (xs + 1) \mapsto{} \mathsf{xs'} \ast{}  \mathrm{list}(\mathsf{new\_tail}, l_1' @ l_2) \}$
   return xs
   # $\{ \mathrm{list}(\mathsf{xs}, l_1 @ l_2) \}$
